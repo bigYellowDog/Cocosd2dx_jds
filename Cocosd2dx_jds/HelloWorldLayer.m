e529deeeb1b54300bc239b846fe416764b420042
bigYellowDog <334068482@qq.com>
2013-11-26 14:25:47 +0800

    第一个版本
    上去吧

---
+++ Cocosd2dx_jds/HelloWorldLayer.m
@@ -0,0 +1,151 @@
+//
+//  HelloWorldLayer.m
+//  Cocosd2dx_jds
+//
+//  Created by Soryin002 on 13-11-26.
+//  Copyright soryin2 2013年. All rights reserved.
+//
+
+
+// Import the interfaces
+#import "HelloWorldLayer.h"
+
+// Needed to obtain the Navigation Controller
+#import "AppDelegate.h"
+
+#pragma mark - HelloWorldLayer
+
+// HelloWorldLayer implementation
+@implementation HelloWorldLayer
+
+// Helper class method that creates a Scene with the HelloWorldLayer as the only child.
++(CCScene *) scene
+{
+	// 'scene' is an autorelease object.
+	CCScene *scene = [CCScene node];
+	// 'layer' is an autorelease object.
+	HelloWorldLayer *layer = [HelloWorldLayer node];
+	
+	// add layer as a child to scene
+	[scene addChild: layer];
+	
+    //adfa
+	// return the scene
+	return scene;
+}
+
+// on "init" you need to initialize your instance
+-(id) init
+{
+	// always call "super" init
+	// Apple recommends to re-assign "self" with the "super's" return value
+	if( (self=[super init]) ) {
+        
+        CCSprite *backgroud=[CCSprite spriteWithFile:@"3.jpg" rect:CGRectMake(0,0, 480, 320)];
+        backgroud.position=ccp(backgroud.contentSize.width/2,320/2);
+        [self addChild:backgroud];
+		// create and initialize a Label
+		CCLabelTTF *label = [CCLabelTTF labelWithString:@"黑帮故事" fontName:@"Marker Felt" fontSize:64];
+        [label setColor:ccc3(41, 128, 185)];
+		// ask director for the window size
+		CGSize size = [[CCDirector sharedDirector] winSize];
+	
+		// position the label on the center of the screen
+		label.position =  ccp( size.width /2 , 180 );
+		
+		// add the label as a child to this Layer
+		[self addChild: label];
+		
+		
+		
+		//
+		// Leaderboards and Achievements
+		//
+		
+		// Default font size will be 28 points.
+		[CCMenuItemFont setFontSize:28];
+		
+		// to avoid a retain-cycle with the menuitem and blocks
+		__block id copy_self = self;
+		
+		// Achievement Menu Item using blocks
+		CCMenuItem *itemAchievement = [CCMenuItemFont itemWithString:@"开始游戏" block:^(id sender) {
+			
+			
+			GKAchievementViewController *achivementViewController = [[GKAchievementViewController alloc] init];
+			achivementViewController.achievementDelegate = copy_self;
+			
+			AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+			
+			[[app navController] presentModalViewController:achivementViewController animated:YES];
+			
+			[achivementViewController release];
+		}];
+		
+		// Leaderboard Menu Item using blocks
+		CCMenuItem *itemLeaderboard = [CCMenuItemFont itemWithString:@"退出游戏" block:^(id sender) {
+			
+			
+			GKLeaderboardViewController *leaderboardViewController = [[GKLeaderboardViewController alloc] init];
+			leaderboardViewController.leaderboardDelegate = copy_self;
+			
+			AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+			
+			[[app navController] presentModalViewController:leaderboardViewController animated:YES];
+			
+			[leaderboardViewController release];
+		}];
+
+		
+		CCMenu *menu = [CCMenu menuWithItems:itemAchievement, itemLeaderboard, nil];
+		
+		[menu alignItemsHorizontallyWithPadding:20];
+		[menu setPosition:ccp( size.width/2, size.height/2 - 50)];
+		
+		// Add the menu to the layer
+		[self addChild:menu];
+
+	}
+	return self;
+}
+
+// on "dealloc" you need to release all your retained objects
+- (void) dealloc
+{
+	// in case you have something to dealloc, do it in this method
+	// in this particular example nothing needs to be released.
+	// cocos2d will automatically release all the children (Label)
+	
+	// don't forget to call "super dealloc"
+	[super dealloc];
+}
+
+#pragma mark GameKit delegate
+
+-(void) achievementViewControllerDidFinish:(GKAchievementViewController *)viewController
+{
+	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+	[[app navController] dismissModalViewControllerAnimated:YES];
+}
+
+-(void) leaderboardViewControllerDidFinish:(GKLeaderboardViewController *)viewController
+{
+	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+	[[app navController] dismissModalViewControllerAnimated:YES];
+}
+
+-(void)addTarget
+{
+    
+}
+
+
+
+
+
+
+
+
+
+
+@end
---
+++ Cocosd2dx_jds/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cocosd2dx_jds
+//
+//  Created by Soryin002 on 13-11-26.
+//  Copyright soryin2 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
